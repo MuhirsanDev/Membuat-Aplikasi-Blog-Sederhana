1dbaf33d381c8454918614639fc825931c136623
Muhirsan <muhirsan.id@gmail.com>
2024-04-28 21:36:34 +0700

    Membuat Tampilan Halaman Blog

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    
+    <h1>halaman Blog</h1>
+    <article>Judul 1</article>
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi praesentium sed architecto. Suscipit perferendis, dolores commodi quaerat architecto nihil atque laudantium maxime laboriosam eaque blanditiis expedita exercitationem nam soluta rem!</h3>
+    
+    <article>Judul 2</article>
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi praesentium sed architecto. Suscipit perferendis, dolores commodi quaerat architecto nihil atque laudantium maxime laboriosam eaque blanditiis expedita exercitationem nam soluta rem!</h3>
+
+</body>
+</html>
