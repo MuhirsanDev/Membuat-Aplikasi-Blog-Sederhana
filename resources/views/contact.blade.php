19ecdb15fc5ccd6635c10f10adfcd2721bef6800
Muhirsan <muhirsan.id@gmail.com>
2024-05-03 10:45:05 +0700

    Membuat Link Navbar Menjadi Lebih Dynamis

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-    
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li><a href="https://www.instagram.com/muhirsannnn">Instagram</a></li>
-        <li><a href="https://www.facebook.com/Muhirsan">Facebook</a></li>
-        <li><a href="https://www.youtube.com/Muhirsan">Youtube</a></li>
-    </ul>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3> Contact Me</h3>
+  </x-layout>
+  
+  
